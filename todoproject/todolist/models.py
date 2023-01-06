1b63c559013f334f835d9aa7c2a90bf93da0f28b
Céline Sèbe <celine.sebe@gmail.com>
2023-01-06 17:07:04 +0100

    admin site register

--- todoproject/todolist/models.py
+++ todoproject/todolist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDo (models.Model):
+    """Model for task """
+    todo_text = models.CharField(verbose_name="Title", max_length=20)
+    deadline = models.DateField(null= True, blank=True)
+    completed = models.BooleanField()
